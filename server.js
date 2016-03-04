5eb86e4842992587bb686244f3aeb12fe39f930d
Brett Gruenler <brett.gruenler@gmail.com>
2016-03-04 00:14:56 -0700

    port for heroku

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middware');
 
 app.use(middleware.logger);
 
-app.get('/about',middleware.requireAuth, function (req, res) {
+app.get('/about', middleware.requireAuth, function(req, res) {
   res.send('About us!')
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
   console.log('express server started on port ' + PORT);
 });
